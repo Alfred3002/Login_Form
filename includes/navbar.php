8511c1bfc8bea27fd24db276514012718ee67499
Alfred3002 <orapaalfred19@gmail.com>
2024-09-20 08:37:26 +0100

    commit the changes

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,42 +1,42 @@
-<div class="bg-dark"></div>
+<div class="bg-secondary-subtle"></div>
 <div class="container">
     <div class="row">
         <div class="col-md-12">
-        <nav class="navbar navbar-expand-lg bg-navbar-dark">
-        <div class="container-fluid">
+        <nav class="navbar navbar-expand-lg bg-secondary-subtle">
+            <div class="container-fluid">
 
-            <a class="navbar-brand" href="#">Simple Login Form</a>
+                <a class="navbar-brand" href="#">Simple Login Form</a>
 
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                <a class="nav-link active" href="index.php">Home</a>
-                </li>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                    <a class="nav-link active" href="index.php">Home</a>
+                    </li>
 
-                <li class="nav-item">
-                <a class="nav-link" href="register.php">Register</a>
-                </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="register.php">Register</a>
+                    </li>
 
-                <li class="nav-item">
-                <a class="nav-link" href="login.php">Login</a>
-                </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="login.php">Login</a>
+                    </li>
 
-                <li class="nav-item">
-                <a class="nav-link" href="dashboard.php">Dashboard</a>
-                </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="dashboard.php">Dashboard</a>
+                    </li>
 
-                <li class="nav-item">
-                <a class="nav-link" href="about.php">About</a>
-                </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="about.php">About</a>
+                    </li>
 
-            </ul>
-            </div>
-        </div>
-</nav>
+                </ul>
+                </div>
+            </div>  
+        </nav>
         </div>
     </div>
 </div>
