de6d4aa13024ce90fd3906903d7fa586e6adddb9
Alfred3002 <orapaalfred19@gmail.com>
2024-09-20 11:29:09 +0100

    partial commit changes

---
+++ code.php
@@ -0,0 +1,17 @@
+<?php
+if(isset($_POST['register_btn'])){
+
+    $name = $_POST['name'];
+    $phone = $_POST['phone'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $confirm_password = $_POST['confirm_password'];
+
+    //check if the email exist or not.
+    $check_email_query = "SELECT email FROM users WHERE email='$email' LIMIT 1";
+    $check_email_query_run = mysqli_query($con, $check_email_query);
+
+   
+
+}
+?>
--- index.php
+++ index.php
@@ -8,9 +8,9 @@ include('includes/navbar.php');
     <div class="py-5">
         <div class="container">
             <div class="row">
-                <div class="col-md-02">
-                    <h4>Login and Registration System in PHP</h4>
-                    <h5>with Email Verification</h5>
+                <div class="col-md-12 text-center">
+                    <h3>Login and Registration System in PHP</h3>
+                    <h4>with Email Verification</h4>
                 </div>
             </div>
         </div>
