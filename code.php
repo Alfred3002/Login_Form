dbe424fc971cbda79ef826bd1e87d2c822c156ff
Alfred3002 <orapaalfred19@gmail.com>
2024-09-27 08:46:15 +0100

    commit changes of this folder

--- code.php
+++ code.php
@@ -17,12 +17,12 @@ require 'vendor/autoload.php';
         $mail->isSMTP(); //Send using SMTP
         $mail->SMTPAuth   = true;  //Enable SMTP authentication
 
-        $mail->Host       = "smtp.example.com"; //Set the SMTP server to send through
+        $mail->Host       = "smtp.gmail.com"; //Set the SMTP server to send through
         $mail->Username   = "shinayang41@gmail.com";  //SMTP username
-        $mail->Password   = "pedoalfred21"; //SMTP password
+        $mail->Password   = "nspqmhlcmxjavkxi"; //SMTP password
 
-        $mail->SMTPSecure = "tls"; //Enable implicit TLS encryption
-        $mail->Port       = 587;  //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+        $mail->SMTPSecure = "ssl"; //Enable implicit TLS encryption
+        $mail->Port       = 465;  //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
         //Recipients
         $mail->setFrom("shinayang41@gmail.com", $name);
@@ -82,5 +82,6 @@ require 'vendor/autoload.php';
 
             }
        }
-     }
+     
+    }
 ?>
