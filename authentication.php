b99715918ff1f64a335cbbeae75e47c0de737aef
Alfred3002 <orapaalfred19@gmail.com>
2024-11-14 13:03:14 +0000

    Change dashborad

--- authentication.php
+++ authentication.php
@@ -3,7 +3,7 @@ session_start();
 
     if(!isset($_SESSION['authenticated'])){
 
-        $_SESSION['status'] = "Please Login to access User's Dashboard.";
+        $_SESSION['status'] = "Please Login to access the System";
         header('Location: login.php');
         exit(0);
 
